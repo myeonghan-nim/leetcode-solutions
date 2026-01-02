c903d1efea027e6304fa1c4d0ab643a34a5be66b
myeonghan-nim <hannim0828@gmail.com>
2026-01-02 19:46:15 +0900

    fix: lint

--- easy/valid_parentheses.py
+++ easy/valid_parentheses.py
@@ -1,11 +1,11 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
-        mapping = {')': '(', '}': '{', ']': '['}
+        mapping = {")": "(", "}": "{", "]": "["}
 
         for char in s:
             if char in mapping:
-                top_element = stack.pop() if stack else '#'
+                top_element = stack.pop() if stack else "#"
                 if mapping[char] != top_element:
                     return False
             else:
