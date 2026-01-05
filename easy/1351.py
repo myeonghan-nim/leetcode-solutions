8bfc72ec1f4a146e94cbf2a516e4baeb746670ab
myeonghan-nim <hannim0828@gmail.com>
2026-01-06 00:38:23 +0900

    chnage: problem solution type

--- easy/1351.py
+++ easy/1351.py
@@ -1,14 +1,10 @@
 class Solution:
     def countNegatives(self, grid: List[List[int]]) -> int:
         count = 0
-        rows = len(grid)
-        cols = len(grid[0]) if rows > 0 else 0
-
-        for r in range(rows):
-            for c in range(cols - 1, -1, -1):
-                if grid[r][c] < 0:
+        for row in grid:
+            for c in row[::-1]:
+                if c < 0:
                     count += 1
                 else:
                     break
-
         return count
--- easy/14.py
+++ easy/14.py
@@ -4,7 +4,6 @@ class Solution:
             return ""
 
         prefix = strs[0]
-
         for s in strs[1:]:
             while not s.startswith(prefix):
                 prefix = prefix[:-1]
