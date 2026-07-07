68b2a396d56ec88db091ec2f0900f5c14d3cb0a1
myeonghan <52690495+myeonghan-nim@users.noreply.github.com>
2026-07-07 13:40:27 +0900

    feat: add 3754 solution

---
+++ easy/3754.py
@@ -0,0 +1,7 @@
+class Solution:
+    def sumAndMultiply(self, n: int) -> int:
+        digits = [int(d) for d in str(n) if d != "0"]
+        if not digits:
+            return 0
+        x = int("".join(map(str, digits)))
+        return x * sum(digits)
